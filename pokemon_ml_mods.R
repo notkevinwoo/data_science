9f574e8abcb4ad9ecae37feca5048f01bc4169e3
notkevinwoo <66450612+notkevinwoo@users.noreply.github.com>
2022-05-09 23:48:13 -0500

    added comments to eda

--- pokemon_ml_mods.R
+++ pokemon_ml_mods.R
@@ -13,9 +13,13 @@ pokemon <- read_csv("data sets/Pokemon.csv") %>%
 # predict legendary pokemon using different classification models
 
 # eda ---------------------------------------------------------------------
-pokemon %>% 
+
+# count and percentage of legendary pokemon
+pokemon_legendary <- pokemon %>% 
   count(legendary) %>% 
-  mutate(percent = n/sum(n)) %>% 
+  mutate(percent = n/sum(n)) 
+
+pokemon_legendary %>% 
   ggplot(aes(x=legendary, y=percent)) + 
   geom_col() + 
   scale_y_continuous(labels = scales::percent) +
@@ -24,28 +28,32 @@ pokemon %>%
        y = "Percent") + 
   coord_flip()
 
-pokemon %>% 
+# legendary by generation
+pokemon_legendary_gen <- pokemon %>% 
   count(legendary, generation) %>% 
   mutate(percent = n/sum(n)) %>% 
-  filter(legendary == TRUE) %>% 
+  filter(legendary == TRUE) 
+
+pokemon_legendary_gen %>% 
   ggplot(aes(x=generation, y=percent)) + 
   geom_col() + 
-  scale_y_continuous(labels = scales::percent) +
-  theme_minimal() + 
-  labs(x = "Legendary",
-       y = "Percent")
+  scale_y_continuous("Percent" ,labels = scales::percent) +
+  scale_x_continuous("Generation", breaks = c(1:6)) +
+  theme_minimal()
 
+# correlation for all pokemon stats
 pokemon_cor <- pokemon %>% 
   select(total:speed) %>% 
   cor()
 
 pokemon_cor %>% 
   corrplot()
-  
+
+# correlation, histogram, and scatter plot of stats  
 ggpairs(pokemon,
         columns = 5:11,
         aes(color = legendary,
-            alpha = 0.8)) + 
+            alpha = 0.2)) + 
   theme_minimal()
 
 
